af0f7d7215c390822d24616cf56f4734ed8e869e
$ ruby -v <ruby -v>
2020-07-30 13:38:29 +0900

    [Finish]5-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'books#top'
+  root 'home#top'
+  get 'home/about' => 'home#about'
+  devise_for :users, controllers: {registrations: 'users/registrations', sessions: 'users/sessions'}
+  resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
