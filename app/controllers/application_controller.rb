887da270b9ec3c7371f879ecad1c7c16825980b0
$ ruby -v <i.i.i.you.vvv.333@gmail.com>
2020-08-20 19:10:34 +0900

    [Add]prefecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :postal_code, :prefecture_code, :city, :building])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_image, :introduction])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_102919) do
+ActiveRecord::Schema.define(version: 2020_08_20_075100) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -53,6 +53,10 @@ ActiveRecord::Schema.define(version: 2020_08_17_102919) do
     t.datetime "updated_at", null: false
     t.text "introduction"
     t.string "profile_image_id"
+    t.string "prefecture_code"
+    t.string "postal_code"
+    t.string "city"
+    t.string "building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
